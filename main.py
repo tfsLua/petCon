24342984185fdc3f5dfeb8ef851cb3d9376bd217
Jácio Alves <163608767+JacioAlvesADS@users.noreply.github.com>
2024-05-13 21:29:00 -0300

    atualizacao das partes do codigo para adicionar as ações do log

--- main.py
+++ main.py
@@ -29,10 +29,13 @@ usuarios_cadastrados =[]
 
 
 class Abrigo:
-    def __init__(self, abrigoName, contato, endereco):
+    def __init__(self, abrigoName, contato, endereco, foto):
         self.abrigoName = abrigoName
         self.contato = contato
         self.endereco = endereco
+        self.foto = foto
+
+
 
 def cadastrar_abrigo():
     abrigoName = input("Qual o nome do seu abrigo? ")
@@ -42,6 +45,8 @@ def cadastrar_abrigo():
     novo_abrigo = Abrigo(abrigoName, contato, endereco)
 
     return novo_abrigo
+
+abrigos_cadastrados = []
 
 
 def login_usuario(usuarios):
@@ -59,7 +64,28 @@ def login_usuario(usuarios):
                 else:
                     print("Senha ou Email incorretos, refaça o login")
                     break
-            
+
+def opcao1_logado():
+    print("voce escolheu 1")
+    # PARTE DO CODIGO PARA ADICIONAR UM NOVO PET
+def opcao2_logado():
+    print("você escolheu 2")
+    # PARTE DO CODIGO PARA ADOTAR UM PET
+def opcao3_logado():
+    print("você escolheu 3")
+    # PARTE DO CODIGO PARA ALTERAR INFORMACOES DO PET 
+def opcao4_logado():
+    print("você escolheu 4")
+    # PARTE DO CODIGO PARA EXCLUIR UM PET
+def opcao5_logado():
+    print("você escolheu 5")
+    # PARTE DO CODIGO PARA VER OS PETS DISPONIVEIS PARA ADOÇÃO
+def opcao6_logado():
+    print("você escolheu 6")
+    # PARTE DO CODIGO PARA VER ABRIGOS DISPONIVEIS
+def opcao7_logado():
+    print("você escolheu 7")      
+    # PARTE DO CODIGO PARA SAIR     
 
 def opcao1():
     print("Bem vindo a nossa plataforma.")
@@ -73,14 +99,52 @@ def opcao2():
     usuario_logado = login_usuario(usuarios_cadastrados)
     if usuario_logado:
         print("Login bem-sucedido!")
+
+        while True:
+            print(" \n 1 - Adicionar novo Pet \n \n 2 - Adotar um Pet \n \n 3 - Alterar informações do pet \n \n 4 - Excluir um Pet \n \n 5 - Ver os pets disponiveis para adoção \n \n 6 - Ver abrigos disponíveis \n \n 7 - Encerrar a Sessão \n")
+            escolha_logado = input()
+            
+            if escolha_logado == '1':
+                opcao1_logado()
+            elif escolha_logado == '2':
+                opcao2_logado()
+
+            elif escolha_logado == '3':
+                opcao3_logado()
+        
+            elif escolha_logado == '4':
+                opcao4_logado()
+        
+            elif escolha_logado == '5':
+                opcao5_logado()
+        
+            elif escolha_logado == '6':
+                opcao6_logado()
+        
+            elif escolha_logado == '7':
+                opcao7_logado()
+        
+            else:
+                print("Digite um valor válido")
+  
+    
+        
+
+        
+
+
+
+
         
     else:
         print("Falha no login. Verifique suas credenciais e tente novamente.")
 
 
 def opcao3():
-    print("Você escolheu a opção 3.")
+    novo_abrigo = cadastrar_abrigo()
+    abrigos_cadastrados.append(novo_abrigo)
 
+    
 
 while True:
     print("Bem vindo a Pet.Con o que você deseja fazer?")
@@ -97,5 +161,4 @@ while True:
    
     else:
         print("Opção inválida.")
-
 
