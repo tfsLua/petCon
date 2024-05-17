a9b3921662f84db13eac1921077df24582221be5
Jácio Alves <163608767+JacioAlvesADS@users.noreply.github.com>
2024-05-17 11:44:39 -0300

    add "adicionar pets" "visualizar lista de pets"
    tambem adicionado um login base pra não precisar cadastrar conta todas as runs

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-''' 
-Nosso projeto da Pet.Con vai entrar aqui tá. As divisões vão ser feitas por sessão.
-
-'''
-
 ''' LOGIN '''
 
 class Usuario:   
-    def __init__(self, firstName, lastName,  email, senha):
+    def __init__(self, firstName, lastName, email, senha):
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
@@ -14,7 +9,6 @@ class Usuario:
 
     def confirmar_senha(self, senha_verificacao):
         return self.senha == senha_verificacao 
-
 
 def cadastrar_usuario():
     firstName = input("Digite o nome do usuário: ")
@@ -25,140 +19,146 @@ def cadastrar_usuario():
     novo_usuario = Usuario(firstName, lastName, email, senha)
     return novo_usuario
 
-usuarios_cadastrados =[]
+usuarios_cadastrados = []
+usuario_predefinido = Usuario("jacio", "filho", "jacioalves6", "jacioalves")
 
-
+# Adicionar o usuário pré-definido à lista de usuários cadastrados
+usuarios_cadastrados.append(usuario_predefinido)
 class Abrigo:
-    def __init__(self, abrigoName, contato, endereco, foto):
+    def __init__(self, abrigoName, contato, endereco, foto=None):
         self.abrigoName = abrigoName
         self.contato = contato
         self.endereco = endereco
         self.foto = foto
 
-
-
 def cadastrar_abrigo():
     abrigoName = input("Qual o nome do seu abrigo? ")
     contato = input("Número de contato: ")
     endereco = input("Endereço do abrigo: ")
+    foto = input("Link da foto do abrigo (opcional): ")
     
-    novo_abrigo = Abrigo(abrigoName, contato, endereco)
-
+    novo_abrigo = Abrigo(abrigoName, contato, endereco, foto)
     return novo_abrigo
 
 abrigos_cadastrados = []
 
+class Pet:
+    def __init__(self, nome, idade, raca, abrigo):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+        self.abrigo = abrigo
+
+pets_cadastrados = []
+
+def cadastrar_pet():
+    nome = input("Nome do pet: ")
+    idade = input("Idade do pet: ")
+    raca = input("Raça do pet: ")
+    abrigo = input("Abrigo onde o pet está: ")
+    
+    novo_pet = Pet(nome, idade, raca, abrigo)
+    pets_cadastrados.append(novo_pet)
+    print("Pet cadastrado com sucesso!")
 
 def login_usuario(usuarios):
     email = input("Digite seu email: ")
-   
+    senha_passada = input("Digite a senha: ")
 
-    while True: 
-        senha_passada = input("Digite a senha: ")
-        for usuario  in usuarios:
-            if usuario.email == email:
-                if usuario.confirmar_senha(senha_passada):
-                    print("Bem vindo a nossa plataforma!")
-                    return usuario
-                
-                else:
-                    print("Senha ou Email incorretos, refaça o login")
-                    break
+    for usuario in usuarios:
+        if usuario.email == email:
+            if usuario.confirmar_senha(senha_passada):
+                print("Bem-vindo(a) à nossa plataforma!")
+                return usuario
+            else:
+                print("Senha incorreta. Tente novamente.")
+                return None
+    print("Email não encontrado. Tente novamente.")
+    return None
 
 def opcao1_logado():
-    print("voce escolheu 1")
-    # PARTE DO CODIGO PARA ADICIONAR UM NOVO PET
+    print("Adicione um pet!")
+    cadastrar_pet()
+
 def opcao2_logado():
-    print("você escolheu 2")
-    # PARTE DO CODIGO PARA ADOTAR UM PET
+    print("Você escolheu 2")
+    # PARTE DO CÓDIGO PARA ADOTAR UM PET
+
 def opcao3_logado():
-    print("você escolheu 3")
-    # PARTE DO CODIGO PARA ALTERAR INFORMACOES DO PET 
+    print("Você escolheu 3")
+    # PARTE DO CÓDIGO PARA ALTERAR INFORMAÇÕES DO PET 
+
 def opcao4_logado():
-    print("você escolheu 4")
-    # PARTE DO CODIGO PARA EXCLUIR UM PET
+    print("Você escolheu 4")
+    # PARTE DO CÓDIGO PARA EXCLUIR UM PET
+
 def opcao5_logado():
-    print("você escolheu 5")
-    # PARTE DO CODIGO PARA VER OS PETS DISPONIVEIS PARA ADOÇÃO
+    print("Pets disponiveis para adoção")
+    if not pets_cadastrados:
+        print("Não há pets disponíveis para adoção no momento.")
+    else:
+        print("Pets disponíveis para adoção:")
+        for pet in pets_cadastrados:
+            print(f"Nome: {pet.nome}, Idade: {pet.idade}, Raça: {pet.raca}, Abrigo: {pet.abrigo}")
+
 def opcao6_logado():
-    print("você escolheu 6")
-    # PARTE DO CODIGO PARA VER ABRIGOS DISPONIVEIS
+    print("Você escolheu 6")
+    # PARTE DO CÓDIGO PARA VER ABRIGOS DISPONÍVEIS
+
 def opcao7_logado():
-    print("você escolheu 7")      
-    # PARTE DO CODIGO PARA SAIR     
+    print("Você escolheu 7")
+    # PARTE DO CÓDIGO PARA SAIR     
 
 def opcao1():
-    print("Bem vindo a nossa plataforma.")
+    print("Bem-vindo(a) à nossa plataforma.")
     novo_usuario = cadastrar_usuario()
     usuarios_cadastrados.append(novo_usuario)
+    print("Usuário cadastrado com sucesso!")
    
-    
 def opcao2():
-   
     print("Você escolheu a opção 2.")
     usuario_logado = login_usuario(usuarios_cadastrados)
     if usuario_logado:
-        print("Login bem-sucedido!")
-
         while True:
-            print(" \n 1 - Adicionar novo Pet \n \n 2 - Adotar um Pet \n \n 3 - Alterar informações do pet \n \n 4 - Excluir um Pet \n \n 5 - Ver os pets disponiveis para adoção \n \n 6 - Ver abrigos disponíveis \n \n 7 - Encerrar a Sessão \n")
+            print("\nEscolha uma opção:\n1 - Adicionar novo Pet\n2 - Adotar um Pet\n3 - Alterar informações do pet\n4 - Excluir um Pet\n5 - Ver os pets disponíveis para adoção\n6 - Ver abrigos disponíveis\n7 - Encerrar a Sessão\n")
             escolha_logado = input()
             
             if escolha_logado == '1':
                 opcao1_logado()
             elif escolha_logado == '2':
                 opcao2_logado()
-
             elif escolha_logado == '3':
                 opcao3_logado()
-        
             elif escolha_logado == '4':
                 opcao4_logado()
-        
             elif escolha_logado == '5':
                 opcao5_logado()
-        
             elif escolha_logado == '6':
                 opcao6_logado()
-        
             elif escolha_logado == '7':
                 opcao7_logado()
-        
+                break
             else:
-                print("Digite um valor válido")
-  
-    
-        
-
-        
-
-
-
-
-        
+                print("Opção inválida. Tente novamente.")
     else:
         print("Falha no login. Verifique suas credenciais e tente novamente.")
 
-
 def opcao3():
+    print("Você escolheu a opção 3.")
     novo_abrigo = cadastrar_abrigo()
     abrigos_cadastrados.append(novo_abrigo)
-
-    
+    print("Abrigo cadastrado com sucesso!")
 
 while True:
-    print("Bem vindo a Pet.Con o que você deseja fazer?")
-    print("\n1 - Cadastrar novo Usuário\n  \n2 - Login\n \n3 - Cadastrar novo Abrigo\n")
+    print("Bem-vindo(a) à Pet.Con! O que você deseja fazer?")
+    print("\n1 - Cadastrar novo Usuário\n2 - Login\n3 - Cadastrar novo Abrigo\n")
     escolha = input()
 
-    if  escolha == '1':
+    if escolha == '1':
         opcao1()
     elif escolha == '2':
         opcao2()
-        break
     elif escolha == '3':
         opcao3()
-   
     else:
-        print("Opção inválida.")
-
+        print("Opção inválida. Tente novamente.")
